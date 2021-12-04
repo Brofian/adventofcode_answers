ccb46e0247aecd6a2538ea0031b8fa9fefa13b63
Fabian Holzwarth <holzwarth.fabian@freenet.de>
2021-12-04 23:48:41 +0100

    [TASK] Added 2020/03

---
+++ aoc/y2020/day03_1.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace aoc\y2020;
+
+use src\AbstractRiddle;
+
+class day03_1 extends AbstractRiddle {
+
+    function getRiddleDescription(): string
+    {
+        return 'Starting at the top-left corner of your map and following a slope of right 3 and down 1, how many trees would you encounter?';
+    }
+
+    function getRiddleAnswer(): string
+    {
+        $lines = $this->readLinesOfFile(__DIR__ . '/files/day03.txt', (function($line) {return trim($line);}));
+        $numLines = count($lines);
+
+        $posX = 0;
+        $posY = 0;
+        $hits = 0;
+        while($posY < $numLines) {
+            $plane = str_split($lines[$posY]);
+            $obstaclePosition = $posX % count($plane);
+            $obstacle = $plane[$obstaclePosition];
+
+            if($obstacle == '#') {
+                $hits++;
+            }
+
+            $posX += 3;
+            $posY += 1;
+        }
+
+
+        return "$hits";
+    }
+
+}
---
+++ aoc/y2020/day03_2.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace aoc\y2020;
+
+use src\AbstractRiddle;
+
+class day03_2 extends AbstractRiddle {
+
+    function getRiddleDescription(): string
+    {
+        return 'What do you get if you multiply together the number of trees encountered on each of the listed slopes?';
+    }
+
+    function getRiddleAnswer(): string
+    {
+        $lines = $this->readLinesOfFile(__DIR__ . '/files/day03.txt', (
+            function($line) {
+                return str_split(trim($line));
+            }
+        ));
+
+        $hits1 = $this->calculateHitsForSlope($lines, 1, 1);
+        $hits2 = $this->calculateHitsForSlope($lines, 3, 1);
+        $hits3 = $this->calculateHitsForSlope($lines, 5, 1);
+        $hits4 = $this->calculateHitsForSlope($lines, 7, 1);
+        $hits5 = $this->calculateHitsForSlope($lines, 1, 2);
+
+
+
+        return $hits1*$hits2*$hits3*$hits4*$hits5;
+    }
+
+
+    protected function calculateHitsForSlope(array $plains, int $slopeX, int $slopeY): int {
+        $posX = 0;
+        $posY = 0;
+        $hits = 0;
+
+        while($posY < count($plains)) {
+            $plane = $plains[$posY];
+            $obstaclePosition = $posX % count($plane);
+            $obstacle = $plane[$obstaclePosition];
+
+            if($obstacle == '#') {
+                $hits++;
+            }
+
+            $posX += $slopeX;
+            $posY += $slopeY;
+        }
+
+        return $hits;
+    }
+
+}
