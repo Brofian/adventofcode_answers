a010f7b623327ce85727a08402ef0d83bc870ea4
Fabian Holzwarth <holzwarth.fabian@freenet.de>
2021-12-08 23:01:42 +0100

    [TASK] Added some function descriptions

--- src/functions.php
+++ src/functions.php
@@ -3,13 +3,24 @@
 // this file contains some utility functions, but also definitions for PHP < 8.0
 
 if(!function_exists('str_contains')) {
+    /**
+     * Checks, if the string contains the substring at any position
+     * @param string $string
+     * @param string $substring
+     * @return bool
+     */
     function str_contains(string $string, string $substring): bool {
         return strpos($string, $substring) !== false;
     }
 }
 
 
-
+/**
+ * Takes in two strings and outputs true if they contain the same letters and the same numbers of them. False, if they dont
+ * @param string $string1
+ * @param string $string2
+ * @return bool
+ */
 function str_compare_letters(string $string1, string $string2): bool {
     $string1Arr = str_split($string1);
     sort($string1Arr);
@@ -19,7 +30,12 @@ function str_compare_letters(string $string1, string $string2): bool {
     return empty(array_diff($string1Arr, $string2Arr)) && empty(array_diff($string2Arr, $string1Arr));
 }
 
-
+/**
+ * Removes chars from the given string. The chars are passed as a string themselves
+ * @param string $string
+ * @param string $charsToRemove
+ * @return string
+ */
 function str_remove_chars(string $string, string $charsToRemove): string {
     return str_replace(
         str_split($charsToRemove),
@@ -28,6 +44,11 @@ function str_remove_chars(string $string, string $charsToRemove): string {
     );
 }
 
+/**
+ * Converts an array of integer numbers to a single number by sticking them together and returns it
+ * @param array $intArray
+ * @return int
+ */
 function int_array_to_int(array $intArray): int {
     return (int)implode('', $intArray);
 }
