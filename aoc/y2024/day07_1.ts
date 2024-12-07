cd56e0d127eb297a57e6dbf55b73c8af106cfca0
Fabian Holzwarth <holzwarth.fabian@freenet.de>
2024-12-08 00:23:49 +0100

    feat: adding 2024 day 07

---
+++ aoc/y2024/day07_1.ts
@@ -0,0 +1,79 @@
+import AbstractRiddle from '../../src/JS/AbstractRiddle';
+
+type TestCase = {
+    testValue: number;
+    operands: number[];
+}
+
+type Operator = {(a: number, b: number): number};
+
+
+class Y2024_Day07_1 extends AbstractRiddle {
+
+    riddle: string = "What is their total calibration result?";
+
+    operators: Operator[] = [
+        (a,b) => a+b,
+        (a,b) => a*b,
+    ];
+
+
+    run(input: string[]): number {
+
+        const cases: TestCase[] = input.map(line => {
+            const regex = /^(\d+): (.*)$/;
+            const m = line.match(regex);
+            this.assert(m !== null);
+
+            return {
+                testValue: parseInt(m[1]),
+                operands: m[2].split(' ').map(s => parseInt(s))
+            };
+        });
+
+
+        return cases.reduce((carry, testCase) => {
+            if (this.checkTestCase(testCase)) {
+                return carry + testCase.testValue;
+            }
+            return carry;
+        }, 0);
+    }
+
+
+    checkTestCase(testCase: TestCase): boolean {
+        // the list of values we could have reached by now
+        let intermResults: number[] = [testCase.operands[0]];
+
+        // for every number of the test case
+        for (let i = 1; i < testCase.operands.length; i++) {
+            const nextOperant = testCase.operands[i];
+
+            // for every possible intermedite result, we have calculated before
+            const nextIntermResults: number[] = [];
+            for (const intermResult of intermResults) {
+
+                // apply all the operators and save the results
+                for (const op of this.operators) {
+                    const result = op(intermResult, nextOperant);
+                    if (!nextIntermResults.includes(result) && result <= testCase.testValue) {
+                        nextIntermResults.push(result);
+                    }
+                }
+
+            }
+
+            intermResults = nextIntermResults;
+            if (intermResults.length === 0) {
+                return false; // no possibilities left
+            }
+        }
+
+        return intermResults.includes(testCase.testValue);
+    }
+
+
+}
+
+// noinspection JSUnusedGlobalSymbols
+export default (new Y2024_Day07_1());
