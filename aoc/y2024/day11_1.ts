2b994bf0aa28a3d7103a7935b26ee7e3da5c03fc
Fabian Holzwarth <holzwarth.fabian@freenet.de>
2024-12-11 18:22:42 +0100

    feat: adding 2024 day 11

---
+++ aoc/y2024/day11_1.ts
@@ -0,0 +1,41 @@
+import AbstractRiddle from '../../src/JS/AbstractRiddle';
+import Vector from "../../src/JS/Vector";
+
+class Y2024_Day11_1 extends AbstractRiddle {
+
+    riddle: string = "How many stones will you have after blinking 25 times?";
+
+    run(input: string[]): number {
+        this.assert(input.length === 1);
+
+        const stones: string[] = input[0].split(' ');
+
+        const blinks = 25;
+        for (let blink = 0; blink < blinks; blink++) {
+
+            for (let i = 0; i < stones.length; i++) {
+                const stone = stones[i];
+
+                if (stone === '0') {
+                    stones[i] = '1';
+                }
+                else if (stone.length % 2 === 0) {
+                    stones.splice(i, 1,
+                        parseInt(stone.substring(0,stone.length/2)) + '',
+                        parseInt(stone.substring(stone.length/2)) + '',
+                    );
+                    i++;
+                }
+                else {
+                    stones[i] = (parseInt(stone) * 2024) + '';
+                }
+            }
+        }
+
+        return stones.length;
+    }
+
+}
+
+// noinspection JSUnusedGlobalSymbols
+export default (new Y2024_Day11_1());
---
+++ aoc/y2024/day11_2.ts
@@ -0,0 +1,82 @@
+import AbstractRiddle from '../../src/JS/AbstractRiddle';
+import Vector from "../../src/JS/Vector";
+
+type Stone = {
+    num: string;
+    count: number;
+}
+
+class Y2024_Day11_2 extends AbstractRiddle {
+
+    riddle: string = "How many stones would you have after blinking a total of 75 times?";
+
+    run(input: string[]): number {
+        this.assert(input.length === 1);
+
+        let stones: {[key: string]: Stone} = {}
+
+        input[0].split(' ').forEach(s => {
+            if (stones[s]) {
+                stones[s].count += 1;
+            }
+            else {
+                stones[s] = {
+                    num: s,
+                    count: 1,
+                }
+            }
+        });
+
+
+
+        const blinks = 75;
+        for (let blink = 0; blink < blinks; blink++) {
+            let nextStones: typeof stones = {};
+
+            for (const stone of Object.values(stones)) {
+
+                if (stone.num === "0") {
+                    if (!nextStones["1"]) {
+                        nextStones["1"] = {
+                            num: "1",
+                            count: 0
+                        };
+                    }
+                    nextStones["1"].count += stone.count;
+                }
+                else if (stone.num.length % 2 === 0) {
+                    const firstHalf = parseInt(stone.num.substring(0,stone.num.length/2)) + '';
+                    const secondHalf = parseInt(stone.num.substring(stone.num.length/2)) + '';
+                    for (const target of [firstHalf, secondHalf]) {
+                        if (!nextStones[target]) {
+                            nextStones[target] = {
+                                num: target,
+                                count: 0
+                            };
+                        }
+                        nextStones[target].count += stone.count;
+                    }
+                }
+                else {
+                    const target = (parseInt(stone.num) * 2024) + '';
+                    if (!nextStones[target]) {
+                        nextStones[target] = {
+                            num: target,
+                            count: 0
+                        };
+                    }
+                    nextStones[target].count += stone.count;
+                }
+
+            }
+
+            stones = nextStones;
+        }
+
+        return Object.values(stones).reduce((carry, stone) => carry + stone.count, 0);
+    }
+
+}
+
+// noinspection JSUnusedGlobalSymbols
+export default (new Y2024_Day11_2());
